23abb55cb529db5c36b2cf714f30b2f1683da9fe
ravibharathi-ss <ravibharathi.softsuave@gmail.com>
2026-03-26 14:57:17 +0530

    feat: add `User` class with private `#name` field, getter/setter, and `print` method.

---
+++ scripts/Class.js
@@ -0,0 +1,25 @@
+class User{
+    #name;
+    constructor(name,age){
+        this.#name = name;
+        this.age = age;
+    }
+
+    print(){
+        return   `Name : ${this.#name}, Age : ${this.age}`;
+    }
+
+    get name(){
+        return this.#name;
+    }
+
+    set name(name){
+        this.#name = name;
+    }
+}
+
+let user1 = new User("Bharathi", 22);
+// console.log(user1.print());
+console.log(user1.name);
+user1.name = "Bharath";
+console.log(user1.name);
